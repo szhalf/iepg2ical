5a839a7a151013bb6444c4b27eec6d1f0873570e
SUZUKI <wataru@adrian.local>
2011-04-29 15:29:58 +0900

    Initial Commit

---
+++ iEPG2iCal/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iEPG2iCal
+//
+//  Created by SUZUKI, Wataru on 4/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
