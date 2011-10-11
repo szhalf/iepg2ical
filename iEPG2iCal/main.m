5a35c00e943ef0afd5da58cd5e4ebca7f1cf57c4
wataru <szhalf@gmail.com>
2011-10-11 22:37:10 +0900

    修正

--- iEPG2iCal/main.m
+++ iEPG2iCal/main.m
@@ -16,25 +16,29 @@ void parse (NSData *data);
 
 int main (int argc, const char * argv[]) {
     
-    /*
-     ＮＨＫ総合\t1
-     ＮＨＫ教育\t3
-     日本テレビ\t4
-     テレビ朝日\t5
-     ＴＢＳテレビ\t6
-     テレビ東京\t7
-     フジテレビ\t8
-     ＭＸテレビ\t9
-     */
-    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    NSDictionary *stationNumbers = [NSDictionary dictionaryWithObjectsAndKeys:
+                                    @"1", @"ＮＨＫ総合" 
+                                    ,@"2", @"ＮＨＫ教育" 
+                                    ,@"4", @"日本テレビ" 
+                                    ,@"5", @"テレビ朝日" 
+                                    ,@"6", @"ＴＢＳテレビ"
+                                    ,@"7", @"テレビ東京" 
+                                    ,@"8", @"フジテレビ" 
+                                    ,@"9", @"ＭＸテレビ" 
+                                    , nil];
+    
     
     NSMutableArray *filePaths = [NSMutableArray array];
     
     NSString *searchPath = [@"~/Downloads" stringByExpandingTildeInPath];
     NSString *outputName;
     NSArray *outputArray;
-    [searchPath completePathIntoString:&outputName caseSensitive:NO matchesIntoArray:&outputArray filterTypes:[NSArray arrayWithObject:@"tvpi"]];
+    [searchPath completePathIntoString:&outputName 
+                         caseSensitive:NO
+                      matchesIntoArray:&outputArray
+                           filterTypes:[NSArray arrayWithObject:@"tvpi"]];
     for (NSString *path in outputArray) {
         if ([path.pathExtension isCaseInsensitiveLike:@"tvpi"]) {
             [filePaths addObject:path];
@@ -61,17 +65,27 @@ int main (int argc, const char * argv[]) {
     }
     
     for (TVProgramInfo *program in programs) {
-        program.station = [[program.station 
-                            stringByReplacingOccurrencesOfString:@"SPTV" withString:@"CS"]
-                           stringByReplacingOccurrencesOfString:@"BSDT" withString:@"BS"];
-        program.memo    = [program.memo stringByReplacingOccurrencesOfString:@"<BR>" withString:@"\r\n" options:NSCaseInsensitiveSearch range:NSMakeRange(0, program.memo.length)];
+        NSString *programStation = program.station;
+        programStation = [programStation stringByReplacingOccurrencesOfString:@"SPTV"
+                                                                   withString:@"CS"];
+        programStation = [programStation stringByReplacingOccurrencesOfString:@"BSDT" 
+                                                                   withString:@"BS"];
+        if ([stationNumbers objectForKey:programStation]) {
+            programStation = [stationNumbers objectForKey:programStation];
+        }
+        
+        NSString *memo = program.memo;
+        memo = [memo stringByReplacingOccurrencesOfString:@"<BR>" 
+                                               withString:@"\r\n"
+                                                  options:NSCaseInsensitiveSearch 
+                                                    range:NSMakeRange(0, program.memo.length)];
         
         CalEvent *event = [CalEvent event];
         event.calendar  = calendar;
-        event.title     = [NSString stringWithFormat:@"[%@] %@", program.station, program.title];
+        event.title     = [NSString stringWithFormat:@"[%@] %@", programStation, program.title];
         event.startDate = program.startDateTime;
         event.endDate   = program.endDateTime;
-        event.notes     = program.memo;
+        event.notes     = memo;
         
         NSError *error = nil;
         [calendarStore saveEvent:event span:CalSpanAllEvents error:&error];
