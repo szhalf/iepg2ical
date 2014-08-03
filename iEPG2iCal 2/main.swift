b92d53eed445af8446e7cfea9b66f25df0aab6bf
Wataru SUZUKI <szhalf@gmail.com>
2014-08-03 18:33:18 +0900

    Initial Commit

---
+++ iEPG2iCal 2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iEPG2iCal 2
+//
+//  Created by Wataru SUZUKI on 8/3/14.
+//  Copyright (c) 2014 sz50.com. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
